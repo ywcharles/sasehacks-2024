842ba47b08bf99174d16fff336e6b9c004237f17
cyw37 <cyw37@drexel.edu>
2024-10-06 16:55:50 -0400

    Fixed merge issues

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,57 +1,89 @@
-import React from 'react';
-import { View, Image } from 'react-native';
-import { Text, Button } from 'react-native-paper';
+import React from "react";
+import { View, Image } from "react-native";
+import { Text, Button } from "react-native-paper";
 
 export default function HomeScreen({ navigation }) {
   return (
     <View
       style={{
-        display: 'flex',
-        flexDirection: 'column',
+        display: "flex",
+        flexDirection: "column",
         gap: 10,
       }}
     >
-      <Image style={{ width: "100%", height: 720, position: "absolute", top: 0, left: 0, zIndex: 1}} source={require('../assets/background.png')}/>
+      <Image
+        style={{
+          width: "100%",
+          height: 720,
+          position: "absolute",
+          top: 0,
+          left: 0,
+          zIndex: 1,
+        }}
+        source={require("../assets/background.png")}
+      />
 
       {/* Text */}
       <View
         style={{
           marginTop: "15%",
           gap: 2,
-          zIndex: 2
-        }}>
-          <Text variant="displayLarge" style={{ color: "white", fontWeight: "bold", marginLeft: "5%"}}>Welcome</Text>
-          <Text variant="bodyLarge" style={{ color: "white", marginRight: "5%", marginLeft: "5%"}}>
-            Got a fridge full of myster ingredients? let us do the thinking for you! Snap a photo of what you've got, and we'll whip up 
-            tasty meal ideas in seconds. Say goodbye to "What's for dinner?" and hello to easy, delicious recipes tailored to what's 
-            already in your fridge!
-          </Text>
+          zIndex: 2,
+        }}
+      >
+        <Text
+          variant="displayLarge"
+          style={{ color: "white", fontWeight: "bold", marginLeft: "5%" }}
+        >
+          Welcome
+        </Text>
+        <Text
+          variant="bodyLarge"
+          style={{ color: "white", marginRight: "5%", marginLeft: "5%" }}
+        >
+          Got a fridge full of myster ingredients? let us do the thinking for
+          you! Snap a photo of what you've got, and we'll whip up tasty meal
+          ideas in seconds. Say goodbye to "What's for dinner?" and hello to
+          easy, delicious recipes tailored to what's already in your fridge!
+        </Text>
       </View>
-      <Image style={{ width: "100%", height: "50%", bottom: 0, zIndex: 2 }} source={require('../assets/chef.png')}/>
+      <Image
+        style={{ width: "100%", height: "50%", bottom: 0, zIndex: 2 }}
+        source={require("../assets/chef.png")}
+      />
 
       {/* Buttons */}
-      <View 
-        style={{ 
+      <View
+        style={{
           marginTop: 20,
           zIndex: 2,
-          paddingHorizontal: '10%',
-          justifyContent: 'center',
-        }}>
-        <Button style={{ height: 50, justifyContent: "center" }} buttonColor="#A9DEF9" textColor="#05299E" icon="camera" mode="elevated" onPress={() => navigation.navigate('Camera')}>Get Started</Button>
+          paddingHorizontal: "10%",
+          justifyContent: "center",
+        }}
+      >
         <Button
-        title="Ingredients"
-        onPress={() => navigation.navigate("Ingredients")}
-      />
-      <Button
-        title="Recipes"
-        onPress={() =>
-          navigation.navigate("Recipes", {
-            ingredients: [
-              "banana", "oats", "egg", "honey"
-            ],
-          })
-        }
-      />  
+          style={{ height: 50, justifyContent: "center" }}
+          buttonColor="#A9DEF9"
+          textColor="#05299E"
+          icon="camera"
+          mode="elevated"
+          onPress={() => navigation.navigate("Camera")}
+        >
+          Get Started
+        </Button>
+        <Button
+          title="Ingredients"
+          onPress={() => navigation.navigate("Ingredients")}
+        />
+        <Button
+          title="Recipes"
+          onPress={() =>
+            navigation.navigate("Recipes", {
+              ingredients: ["banana", "oats", "egg", "honey"],
+            })
+          }
+        />
+      </View>
     </View>
   );
 }
