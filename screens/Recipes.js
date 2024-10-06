86054d8e3e66de47dd717748b77f6b50e6199e38
cyw37 <cyw37@drexel.edu>
2024-10-06 09:16:50 -0400

    routing-set

---
+++ screens/Recipes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Recipes({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Recipes</Text>
+    </View>
+  );
+}
