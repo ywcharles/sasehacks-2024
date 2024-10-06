21fcc7ecfb530aedc7a4747433247977f733b792
Kiki Lin <kikilin512@gmail.com>
2024-10-06 14:56:10 -0400

    added texts to homepage and make page prettier

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,40 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, Image } from 'react-native';
+import { Text, Button } from 'react-native-paper';
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Button
-        title="Camera"
-        onPress={() => navigation.navigate('Camera')}
-      />
-      <Button
-        title="Ingredients"
-        onPress={() => navigation.navigate('Ingredients')}
-      />
-      <Button
-        title="Recipes"
-        onPress={() => navigation.navigate('Recipes')}
-      />
+    <View
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 10,
+      }}
+    >
+      <Image style={{ width: "100%", height: 650, position: "absolute"}} source={require('../assets/background.png')}/>
+
+      {/* Text */}
+      <View
+        style={{
+          marginTop: "12%",
+          gap: 2
+        }}>
+          <Text variant="displayMedium" style={{ color: "white", fontWeight: "bold", marginLeft: "3%"}}>Welcome</Text>
+          <Text variant="bodyLarge" style={{ color: "white", marginRight: "5%", marginLeft: "4%"}}>
+            Got a fridge full of myster ingredients? let us do the thinking for you! Snap a photo of what you've got, and we'll whip up 
+            tasty meal ideas in seconds. Say goodbye to "What's for dinner?" and hello to easy, delicious recipes tailored to what's 
+            already in your fridge!
+          </Text>
+      </View>
+      {/* Buttons */}
+      <View 
+        style={{ 
+          gap: 10 , 
+        }}>
+        <Button icon="camera" mode="elevated" onPress={() => navigation.navigate('Camera')}>Camera</Button>
+        <Button mode="elevated" onPress={() => navigation.navigate('Ingredients')}>Ingredients</Button>
+        <Button mode="elevated" onPress={() => navigation.navigate('Recipes')}>Recipes</Button>
+      </View>
     </View>
   );
 }
