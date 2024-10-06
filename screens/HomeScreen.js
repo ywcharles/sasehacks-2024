bbd43e1fa9708e965c0234f03da6194f9bbac75d
Kiki Lin <kikilin512@gmail.com>
2024-10-06 15:35:05 -0400

    added image, prettier the buttons, fixed sizing

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,29 +11,35 @@ export default function HomeScreen({ navigation }) {
         gap: 10,
       }}
     >
-      <Image style={{ width: "100%", height: 650, position: "absolute"}} source={require('../assets/background.png')}/>
+      <Image style={{ width: "100%", height: 720, position: "absolute", top: 0, left: 0, zIndex: 1}} source={require('../assets/background.png')}/>
 
       {/* Text */}
       <View
         style={{
-          marginTop: "12%",
-          gap: 2
+          marginTop: "15%",
+          gap: 2,
+          zIndex: 2
         }}>
-          <Text variant="displayMedium" style={{ color: "white", fontWeight: "bold", marginLeft: "3%"}}>Welcome</Text>
-          <Text variant="bodyLarge" style={{ color: "white", marginRight: "5%", marginLeft: "4%"}}>
+          <Text variant="displayLarge" style={{ color: "white", fontWeight: "bold", marginLeft: "5%"}}>Welcome</Text>
+          <Text variant="bodyLarge" style={{ color: "white", marginRight: "5%", marginLeft: "5%"}}>
             Got a fridge full of myster ingredients? let us do the thinking for you! Snap a photo of what you've got, and we'll whip up 
             tasty meal ideas in seconds. Say goodbye to "What's for dinner?" and hello to easy, delicious recipes tailored to what's 
             already in your fridge!
           </Text>
       </View>
+      <Image style={{ width: "100%", height: "43%", bottom: 0, zIndex: 2 }} source={require('../assets/chef.png')}/>
+
       {/* Buttons */}
       <View 
         style={{ 
-          gap: 10 , 
+          gap: 20,
+          zIndex: 2,
+          paddingHorizontal: '10%',
+          justifyContent: 'center',
         }}>
-        <Button icon="camera" mode="elevated" onPress={() => navigation.navigate('Camera')}>Camera</Button>
-        <Button mode="elevated" onPress={() => navigation.navigate('Ingredients')}>Ingredients</Button>
-        <Button mode="elevated" onPress={() => navigation.navigate('Recipes')}>Recipes</Button>
+        <Button buttonColor="#9ACEEB" textColor="#00008B" icon="camera" mode="elevated" onPress={() => navigation.navigate('Camera')}>Camera</Button>
+        <Button buttonColor="#9ACEEB" textColor="#00008B" mode="elevated" onPress={() => navigation.navigate('Ingredients')}>Ingredients</Button>
+        <Button buttonColor="#9ACEEB" textColor="#00008B"mode="elevated" onPress={() => navigation.navigate('Recipes')}>Recipes</Button>
       </View>
     </View>
   );
